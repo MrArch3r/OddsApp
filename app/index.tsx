8bed1b3f27eea6aadd2aea6c5f62b83a7d50323d
Jordan Archer <z5480104@ad.unsw.edu.au>
2024-12-20 17:04:53 +1100

    Added text and input

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,16 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Text, View, TextInput } from "react-native";
+import { RFPercentage } from "react-native-responsive-fontsize";
 
 export default function Index() {
+  const [percentage, setPercentage] = useState("");
+
+  const handleInputChange = (text: string) => {
+    if (!isNaN(Number(text)) || text === "") {
+      setPercentage(text);
+    }
+  };
+
   return (
     <View
       style={{
@@ -9,7 +19,42 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text
+        style={{
+          width: "60%",
+          fontSize:  RFPercentage(3),
+          textAlign: "center",
+        }}
+      >
+        Enter the probability of the event occuring
+      </Text>
+      <View
+        style={{
+          width: "20%",
+          height: "5%",
+        }}
+      >
+        <TextInput
+          keyboardType="numeric"
+          style={{
+            marginTop: 10,
+            width: "100%",
+            height: "100%",
+            borderWidth: 2,
+            borderColor: "black",
+            borderRadius: 5,
+          }}
+          value={percentage}
+          onChangeText={handleInputChange}
+        />
+        <Text
+          style={{
+            fontSize: RFPercentage(2),
+          }}
+        >
+          %
+        </Text>
+      </View>
     </View>
   );
 }
