764cff5734801c07c766b3ed19fcd749df6591ad
Jordan Archer <z5480104@ad.unsw.edu.au>
2024-12-27 15:34:50 +1100

    Added more messages and changed the message showing logic

--- app/pages/resultsPage.tsx
+++ app/pages/resultsPage.tsx
@@ -5,44 +5,56 @@ import { RFPercentage } from "react-native-responsive-fontsize";
 import { useRouter } from "expo-router";
 
 const oddsMessages: Record<number, string> = {
+  0.0001: "being hit by a falling meteorite",
+  0.001: "being struck by lightning in your lifetime",
+  0.01: "finding a shiny Pokemon",
+  0.1: "giving birth to twins",
   1: "randomly hitting the inner bullseye on a dart board",
   2: "randomly selecting someone with green eyes",
   3: "hitting a specific number in roulette",
   4: "getting a critical hit in Pokemon",
   5: "randomly selecting someone with hazel eyes",
   6: "getting all heads or all tails when flipping 5 coins",
+  7: "randomly selecting a number and it being divisible by 14",
   8: "drawing an ace from a standard deck of cards",
   9: "randomly selecting the letter T from an english text",
-  10: "being left-handed",
+  10: "being born left-handed",
   13: "randomly selecting the letter E from an english text",
   15: "drawing either an ace or 2 from a standard deck",
   17: "rolling a 6 on a fair dice",
-  20: "",
+  20: "randomly selecting a number and it being divisible by 5",
   23: "drawing a picture card from a standard deck",
   24: "missing a penalty kick",
   25: "flipping 2 coins and getting 2 heads",
   27: "being dealt a 21, 20, 19 or 18 from the first two cards in Blackjack",
   31: "selecting either an Ace, King, Queen or Jack from a standard deck of cards",
   33: "winning a game of Rock-Paper-Scissors",
+  38: "an NBA player making a three point shot",
   42: "getting a pair in 5-card poker",
+  48: "not rolling a single 6 from four dice rolls",
   50: "flipping a coin and it landing on heads",
+  52: "rolling at least one 6 from four dice rolls",
+  58: "rolling a 7 or less from two 6 sided dice",
+  62: "an NBA player missing a three point shot",
   67: "not winning a game of Rock-Paper-Scissors",
+  70: "two people having the same birthday in a room of 30 people",
   75: "flipping 2 coins and getting at least 1 head",
   76: "scoring a goal from a penalty kick",
   79: "randomly selecting someone with brown eyes",
   83: "not rolling a 6 on a fair dice",
+  88: "getting at least 1 head in 3 coin flips",
   92: "rolling more than a 3 from a pair of dice",
   97: "getting at least 1 head in 5 coin flips",
   98: "picking someone randomly with an IQ below 130",
-  99: "2 people having the same birthday in a room of 60 people",
+  99: "two people having the same birthday in a room of 60 people",
 }
 
 export default function ResultsPage() {
   const { percentage } = useLocalSearchParams();
   const router = useRouter();
 
-  const p: number = parseFloat(Number(percentage).toFixed(2));
-  const odds: number = parseFloat((100 / p).toFixed(2));
+  const p: number = parseFloat(Number(percentage).toFixed(4));
+  const odds: number = parseFloat((100 / p).toFixed(4));
   let isInt: boolean = false;
 
   const reduceFraction = (numerator: number, denominator: number): [number, number] => {
@@ -65,13 +77,25 @@ export default function ResultsPage() {
   if (Number.isInteger(odds)) {
     isInt = true;
   } else {
-    const [numer, denom] = reduceFraction(p * 100, 10000);
+    const [numer, denom] = reduceFraction(Math.round(p * 100), 10000);
     numerator = numer;
     denominator = denom;
   }
 
   const handleBack = () => {
     router.push("/pages/home");
+  }
+
+  const findClosestKey = (num: number): number => {
+    let minDiff: number = 100;
+    let closestKey: number = -1;
+    for (const key in oddsMessages) {
+      if (Math.abs(Number(key) - num) < minDiff) {
+        minDiff = Math.abs(Number(key) - num);
+        closestKey = Number(key);
+      }
+    }
+    return closestKey;
   }
 
   return (
@@ -91,7 +115,7 @@ export default function ResultsPage() {
         >
           Your odds are 1 in {odds}
         </Text>
-        {!isInt && (
+        {!isInt && numerator !== 1 && (
           <Text
             style={{
               width: "80%",
@@ -103,18 +127,16 @@ export default function ResultsPage() {
           </Text>
         )}
 
-        {Math.round(p) in oddsMessages && (
-          <Text
-            style={{
-              marginTop: RFPercentage(5),
-              width: "80%",
-              fontSize:  RFPercentage(3),
-              textAlign: "center",
-            }}  
-          >
-            This is about as often as {oddsMessages[Math.round(p)]}
-          </Text>
-        )}
+        <Text
+          style={{
+            marginTop: RFPercentage(5),
+            width: "80%",
+            fontSize:  RFPercentage(3),
+            textAlign: "center",
+          }}  
+        >
+          This is about as often as {oddsMessages[findClosestKey(p)]}
+        </Text>
       </View>
       <TouchableOpacity
         style={{
